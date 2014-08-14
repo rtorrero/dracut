80cf554a41b83897b7395d282845ae70c6e71d05
Marcus Meissner <meissner@suse.com>
2014-08-14 16:13:55 +0200

    Switch from Mozilla NSS sha256hmac checking to fipscheck as recommended
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    Ported-to-045-by: Daniel Molkentin <daniel.molkentin@suse.com>

--- modules.d/01fips/fips.sh
+++ modules.d/01fips/fips.sh
@@ -59,9 +59,7 @@ do_rhevh_check()
     kpath=${1}
 
     # If we're on RHEV-H, the kernel is in /run/initramfs/live/vmlinuz0
-    HMAC_SUM_ORIG=$(cat $NEWROOT/boot/.vmlinuz-${KERNEL}.hmac | while read a b || [ -n "$a" ]; do printf "%s\n" $a; done)
-    HMAC_SUM_CALC=$(sha512hmac $kpath | while read a b || [ -n "$a" ]; do printf "%s\n" $a; done || return 1)
-    if [ -z "$HMAC_SUM_ORIG" ] || [ -z "$HMAC_SUM_CALC" ] || [ "${HMAC_SUM_ORIG}" != "${HMAC_SUM_CALC}" ]; then
+    if fipscheck $NEWROOT/boot/vmlinuz-${KERNEL} ; then
         warn "HMAC sum mismatch"
         return 1
     fi
@@ -127,7 +125,7 @@ do_fips()
     elif [ -e "/run/initramfs/live/isolinux/vmlinuz0" ]; then
         do_rhevh_check /run/initramfs/live/isolinux/vmlinuz0 || return 1
     else
-        sha512hmac -c "/boot/.${BOOT_IMAGE}.hmac" || return 1
+        fipscheck "/boot/${BOOT_IMAGE}" || return 1
     fi
 
     info "All initrd crypto checks done"
